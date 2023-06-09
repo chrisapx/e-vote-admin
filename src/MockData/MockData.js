5cfba5c5ad08d6be3bc6f3f76c99056abe7591a9
Chris <96912309+Chris-Mwesigwa@users.noreply.github.com>
2023-06-09 09:34:07 +0300

    project setup and themes

---
+++ src/MockData/MockData.js
@@ -0,0 +1,58 @@
+export const mockData = [
+    {
+        id: 1,
+        name: "Chris",
+        email: "mcaplexya@gmail.com",
+        age: 22,
+        phone: "(+1)76526712",
+        role : "admin"
+    },
+    {
+        id: 2,
+        name: "Aplexy",
+        email: "mcaplexya@gmail.com",
+        age: 22,
+        phone: "(+1)76526712",
+        role : "admin"
+    },
+    {
+        id: 3,
+        name: "Esther",
+        email: "mcaplexya@gmail.com",
+        age: 22,
+        phone: "(+1)76526712",
+        role : "admin"
+    },
+    {
+        id: 4,
+        name: "Vincent",
+        email: "mcaplexya@gmail.com",
+        age: 22,
+        phone: "(+1)76526712",
+        role : "admin"
+    },
+    {
+        id: 5,
+        name: "Joice",
+        email: "mcaplexya@gmail.com",
+        age: 22,
+        phone: "(+1)76526712",
+        role : "admin"
+    },
+    {
+        id: 6,
+        name: "Nalubega",
+        email: "mcaplexya@gmail.com",
+        age: 22,
+        phone: "(+1)76526712",
+        role : "admin"
+    },
+    {
+        id: 7,
+        name: "Francis",
+        email: "mcaplexya@gmail.com",
+        age: 22,
+        phone: "(+1)76526712",
+        role : "admin"
+    }
+]
